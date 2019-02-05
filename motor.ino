e4ac7741018c396d72a60aea0d7190ac97a43c9f
rcollier518 <collierr580@strose.edu>
2019-02-04 19:25:18 -0500

    Add files via upload

---
+++ motor.ino
@@ -0,0 +1,63 @@
+  const int led=9;
+  const int button=8;
+  const int Motor=4;
+   int buttonState=0;
+  int flag=0;
+void setup() {
+  Serial.begin(9600);
+ // Serial.print("in if");
+  pinMode(led,OUTPUT);
+  pinMode(Motor,OUTPUT);
+  pinMode(button,INPUT_PULLUP);
+  
+}
+
+  
+  // put your setup code here, to run once:
+void loop() {
+ 
+  buttonState=digitalRead(button);
+Serial.print(buttonState);
+  if(buttonState==1){
+  //Serial.print("in first if");
+    if(flag==0){
+    //1 = on
+      digitalWrite(led,1);
+
+    flag=1;
+      if(flag==1){
+        delay(50000);
+        //0==off);
+      digitalWrite(led,0);
+    
+      
+         digitalWrite(Motor,1);
+         delay(10000);
+         digitalWrite(Motor,0);
+      }
+     
+     
+       
+        
+        
+        
+        
+       
+      }
+     
+
+  
+    }
+ 
+
+  }
+
+
+
+ 
+  
+
+   
+  // put your main code here, to run repeatedly:
+
+
